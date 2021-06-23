e61f9313a99b75b7a16a48705ffbad5114b7e1c2
Rodrous <59244346+Rodrous@users.noreply.github.com>
2021-06-23 11:24:14 +0530

    Initial Commit

---
+++ Readme.md.md
@@ -0,0 +1,32 @@
+# PPTDumBot
+
+## Things to keep in mind
+
+- DONOT make any changes to the `Procfile` or to `client`
+- requirements will consist of all the external libraries you would  use.
+- Code is messy, i will soon implement DRY, so it will be bit better to read.
+- The only thing needed to be edited is the `dumdiscord.py` script
+
+## How to Add Emojis?
+
+Go [here](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjIjuWghq3xAhXEwzgGHWAiDvkQFnoECAQQAw&url=https%3A%2F%2Funicode.org%2Femoji%2Fcharts%2Ffull-emoji-list.html&usg=AOvVaw2Y8ixSqM60XYhs6auuOUsB), copy the unicode
+
+replace “+” with “000” 
+
+![PPTDumBot%2017652a0bcf3846aca49dc48d4229374d/Untitled.png](PPTDumBot%2017652a0bcf3846aca49dc48d4229374d/Untitled.png)
+
+For example :
+
+“U+1F911” will become “U0001F911” and add a `\` in front of it before printing.
+
+![PPTDumBot%2017652a0bcf3846aca49dc48d4229374d/Untitled%201.png](PPTDumBot%2017652a0bcf3846aca49dc48d4229374d/Untitled%201.png)
+
+## Confused on how to make PRs?
+
+See this [vid](https://www.youtube.com/watch?v=rgbCcBNZcdQ) or read [this](https://scotch.io/tutorials/creating-your-first-pull-request-in-github).
+
+Github cheatsheet: [Git CheatSheet](https://www.notion.so/Git-CheatSheet-914296aee4fc4003b9ae19f3de598ba5) 
+
+---
+
+![PPTDumBot%2017652a0bcf3846aca49dc48d4229374d/Untitled%202.png](PPTDumBot%2017652a0bcf3846aca49dc48d4229374d/Untitled%202.png)
---
+++ dumdiscord.py
@@ -0,0 +1,86 @@
+import discord
+import aiohttp
+import requests
+import io
+import base64
+import random
+
+with open("client.txt") as file:
+    f = file.readline()
+
+id = base64.b64decode(f).decode('utf-8')
+client = discord.Client()
+channels_to_send = ["database"]
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game('Your Mum.'))
+@client.event
+async def on_message(message):
+
+    if str(message.channel) not in channels_to_send: 
+     # Everything Working from Down here
+        if message.content.lower() == "!quote":
+            url = requests.get('https://animechan.vercel.app/api/random').json()
+            await message.channel.send(f"A quote from {url['character']} : {url['quote']}")
+
+        if message.content.lower() == "!cat":
+            get_image_url = requests.get(f"https://aws.random.cat/meow").json()
+            url = get_image_url['file']
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status != 200:
+                        return await message.channel.send("Couldn't download the file")
+                    data = io.BytesIO(await response.read())
+                    await message.channel.send("From PPT with \U0001F49A")
+                    await message.channel.send(file=discord.File(data,"Cat.png"))
+        removed_stuff = ['!waifu','nsfw-fu','wamen']
+        if message.content.lower() in removed_stuff:
+            await message.channel.send("Functionaliy Removed, will be added in Dumbot1.1")
+
+        if message.content.lower() == "!dog":
+            get_image_url = requests.get(f"https://dog.ceo/api/breeds/image/random").json()
+            url = get_image_url['message']
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status != 200:
+                        return await message.channel.send("Couldn't download the file")
+                    data = io.BytesIO(await response.read())
+                    await message.channel.send("From PPT with \U0001F49A")
+                    await message.channel.send(file=discord.File(data,"dog.png"))
+
+        if message.content.lower() == "!ree":
+            num = random.randint(1,100)
+            await message.channel.send("*R"+"E"*num + "*")
+
+        if message.content.lower() == "!help":
+            embed = discord.Embed(title="Bot help perhaps", description="Some useful commands")
+            embed.add_field(name="!ree", value="Rees out of frustration")
+            embed.add_field(name="!s", value="Sends a Random image from internet [1920x1080]")
+            embed.add_field(name= "!cat", value = "Sends a Cat pic \U0001F408")
+            embed.add_field(name= "!quote", value = "Sends a random anime quote!")
+            embed.add_field(name= "!invite", value = "Sends 'add bot to server' link")
+            embed.add_field(name= "!dog", value="Get a Dog pic \U0001F436")
+            await message.channel.send(content=None, embed=embed)
+
+        if message.content.lower() == "!ppt":
+            quote = ["Why do i do shit like this smh!", "This was a mistake", "Leaving this goodbye", "Fuck this shit smh", "Very Carefully Next Question", "Coming to the vc was a mistake", "yo no hablo ingles", "i need some booze", "go make me a sandwitch", "sigh What do you want.", "Because I said so.","ahem Can I help you?", "Peace, dudes."]
+            val =random.choice(quote)
+            embed = discord.Embed(title= val )
+            await message.channel.send(content=None, embed=embed)
+        if message.content.lower() == "!clear":
+            await message.channel.purge(limit=1)
+            await message.channel.send("Command Depricated UwU")
+        if message.content.lower() == "!s":
+            url = f"https://picsum.photos/1920/1080"
+            async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as response:
+                        if response.status != 200:
+                            return await message.channel.send("Couldn't download the file..")
+                        data = io.BytesIO(await response.read())
+                        await message.channel.send(file=discord.File(data,"Some Image.png"))
+        if message.content.lower() == "!invite":
+            await message.channel.send("This was a mistake")
+            await message.channel.send("<https://discordapp.com/oauth2/authorize?client_id=852977382016024646&scope=bot&permissions=0>")
+
+if __name__ == "__main__":
+    client.run(id)
