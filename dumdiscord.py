5e8d02603908f59366f9649c1b41b4e63de86edf
Rodrous <rodrous114@gmail.com>
2021-06-26 22:51:11 +0530

    Removed a Extra print statememnt

--- dumdiscord.py
+++ dumdiscord.py
@@ -20,7 +20,6 @@ with open("config/allowedguildIds.txt") as file:
 
 #allowedguilds = [base64.b64decode(i).decode('utf-8') for i in f]
 allowedguilds = [i for i in f]
-print(allowedguilds)
  # ------------------------------------------------------------------------------------------------------------------ 
 
 # This is how Danny(Creator of python API for discord) handles custom prefixes, but that needs cogs which i have no idea
