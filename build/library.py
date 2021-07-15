7058d9107690888f185a26956035a3fd7a68f843
EvilGiraffes <85831955+EvilGiraffes@users.noreply.github.com>
2021-07-16 01:38:32 +0200

    added a comment to library

--- build/library.py
+++ build/library.py
@@ -1,8 +1,12 @@
 import random
+"""
+Feel free to add as many moviequotes as you want, if there is no character leave it blank but dont remove it
+"""
 async def moviequotes():
     mq = [
         {"movie": "Dirty Harry [1971]", "character": "Harry", "quote": "You've got to ask yourself one question; Do you feel lucky? well, do ya, punk?"},
-        {"movie": "The Godfather", "character": "Don Vito Corleone", "quote": "I'm gonna make him an offer he cant refuse"},
+        {"movie": "The Godfather [1972]", "character": "Don Vito Corleone", "quote": "I'm gonna make him an offer he cant refuse"},
+        {"movie": "Psycho[1960]", "character": "", "quote": "A boy's best friend is his mother"},
         # {"movie": "", "character": "", "quote": ""},
     ]
     return random.choice(mq)
