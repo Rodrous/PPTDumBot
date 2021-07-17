3279ddb7e02fb720f1a40393ee7bf956f755cb86
EvilGiraffes <85831955+EvilGiraffes@users.noreply.github.com>
2021-07-17 15:08:25 +0200

    small changes, not gonna work more on it today

--- cogs/help.py
+++ cogs/help.py
@@ -2,6 +2,21 @@ from discord.ext import commands
 import discord
 import re as reg
 from build.generalPurpose import getPrefix
+
+async def introMenu(prefix):
+    intro = discord.Embed(
+        title="Bot Help Perhaps",
+        description=
+        f"**Server Prefix:** `{prefix}`"
+        "\n*If you need help on a specific command you can type:* `help [command]`"
+        "\n\n**React with:**"
+        "\n<:PepeLmao:865712134439436328> For Fun"
+        "\n<:PepoGamer:865712213141356565> For Games"
+        "\n<a:pepeAnimeCaught:865712704315850782> For Utility"
+        "\n<a:pepeban:865714938667991091> For Moderation"
+        , color=11187115)
+    return intro
+
 class gethelp(commands.Cog):
 
     def __init__(self,bot):
@@ -20,19 +35,8 @@ class gethelp(commands.Cog):
         args = args.split()
         pf = await getPrefix(ctx, self.bot)
         prefix = pf[2]
+        intro = await introMenu(prefix)
         if not args:
-            #INTRO
-            intro = discord.Embed(
-                title="Bot Help Perhaps",
-                description=
-                f"**Server Prefix:** `{prefix}`"
-                "\n*If you need help on a specific command you can type:* `help [command]`"
-                "\n\n**React with:**"
-                "\n<:PepeLmao:865712134439436328> For Fun"
-                "\n<:PepoGamer:865712213141356565> For Games"
-                "\n<a:pepeAnimeCaught:865712704315850782> For Utility"
-                "\n<a:pepeban:865714938667991091> For Moderation"
-                , color=11187115)
             #GENERAL HERE
             #general = discord.Embed(title="", description="", color=)
             #FUN HERE
@@ -175,8 +179,7 @@ class gethelp(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        reaction.message.add_reaction(emoji='<a:bot:855557085495951360>')
-
+        pass
 
 def setup(bot):
     bot.add_cog(gethelp(bot))
