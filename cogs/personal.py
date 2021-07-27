64c87b137b3d038f9b2ef0a6e9367e8ab2702521
EvilGiraffes <85831955+EvilGiraffes@users.noreply.github.com>
2021-07-27 20:23:50 +0200

    Added helpmenu to new personal cog, it is in there at the bottom

--- cogs/personal.py
+++ cogs/personal.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 import discord, random
+from build.generalPurpose import getPrefix
+import re as reg
 
 def devsOnly(ctx):
     devs = {
@@ -34,6 +36,8 @@ def private(ctx):
 
 devsonly = commands.check(devsOnly)
 private = commands.check(private)
+private_embed_color = 6724095
+bot_avatar = 'https://cdn.discordapp.com/avatars/852977382016024646/12f7f96521114553fc7f4b2766dd086f.png?size=2048'
 
 class personal(commands.Cog):
     def __init__(self,bot):
@@ -78,6 +82,65 @@ class personal(commands.Cog):
             for person in pinged_people_list:
                 string += f"<@{str(person)}> "
             await ctx.send(string)
+
+    """    HELP MENU    """
+    @private
+    @commands.command(
+        name="personalhelp",
+        aliases=["phelp", "pershelp", "privhelp", "privatehelp"])
+    async def personalHelp(self, ctx, *, args= ''):
+        await ctx.message.channel.purge(limit=1)
+        args = args.split()
+        pf = await getPrefix(ctx, self.bot)
+        prefix = pf[2]
+        if not args:
+            embed = discord.Embed(
+                title="Private Help Menu",
+                description=
+                f"**For more info on each commands do** `{prefix}phelp [command]`\n*There you will also find Syntax, Aliases and Flags for commands*\n\n"
+                f"`{prefix}ily`: Sends ily cause Draf do be lazy\n"
+                f"`{prefix}vc`: Pings people to vc (On a list, ask to be removed or added)"
+                , color=private_embed_color)
+            embed.set_footer(text="This message will self destruct in 1 minute", icon_url=bot_avatar)
+            await ctx.send(embed=embed, delete_after=60)
+        else:
+            cmd = args[0].lower()
+            aliased_commands = {
+                reg.compile(pattern="FILLER"): "REMOVE WHEN FIRST ALIASED COMMAND IS HERE"
+            }
+            for val in aliased_commands:
+                if reg.match(val, string=cmd):
+                    cmd = aliased_commands.get(val)
+            switch_case = {
+                'ily': {
+                    'desc': 'Sends ily cause Draf is lazy',
+                    'color': private_embed_color
+                },
+                'vc': {
+                    'desc': 'Pings people to vc (On a list, ask to be removed or added)',
+                    'color': private_embed_color
+                },
+                # '': {
+                #     'aliases': '',
+                #     'syntax': f'',
+                #     'desc': 'REQUIRED',
+                #     'color': private_color
+                # },
+            }
+            case = switch_case.get(cmd, None)
+            if case:
+                desc_final = f"**Prefix:** `{prefix}`\n**Name:** `{cmd.capitalize()}`"
+                if case.get('aliases', None):
+                    desc_final = desc_final + f"\n**Aliases:** `{case['aliases']}`"
+                if case.get('syntax', None):
+                    desc_final = desc_final + f"\n**Syntax:** `{case['syntax']}`"
+
+                desc_final = desc_final + f"\n\n{case['desc']}"
+                embed = discord.Embed(title="Command Help", description=desc_final, color=case['color'])
+                embed.set_footer(text="This message will self destruct in 1 minute", icon_url=bot_avatar)
+                await ctx.send(embed=embed, delete_after=60)
+            else:
+                await ctx.send('Not a valid command', delete_after=6)
 
 def setup(bot):
     bot.add_cog(personal(bot))
