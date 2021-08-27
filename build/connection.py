5f53e62bf449d6a5bf4b1b4953c96d8f8703ca7a
Rodrous <rodrous114@gmail.com>
2021-08-27 20:57:31 +0530

    Obfuscated

--- build/connection.py
+++ build/connection.py
@@ -1,9 +1,10 @@
-import pymongo
+import pymongo,base64
 
-def connection():
-    conn_str = "mongodb+srv://bot:bleh123@cluster0.dj6bx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    client = pymongo.MongoClient(conn_str)
-    return client
+#Obfuscated this code because whynot
+def connection ():
+    OOO000O0O0O0O00OO =base64 .b64decode ('bW9uZ29kYitzcnY6Ly9ib3Q6YmxlaDEyM0BjbHVzdGVyMC5kajZieC5tb25nb2RiLm5ldC9teUZpcnN0RGF0YWJhc2U/cmV0cnlXcml0ZXM9dHJ1ZSZ3PW1ham9yaXR5').decode ('Utf-8')
+    O00OOO0O0OOO0O00O =pymongo .MongoClient (OOO000O0O0O0O00OO )
+    return O00OOO0O0OOO0O00O 
 
 
 def createDb(connectionObj,databaseName,collectionName):
