c9ba27022657f16f1df2eedb9bbf27d813fadc4d
rodrous <rodrous114@gmail.com>
2022-01-08 22:46:37 +0530

    Added a few new things, waiting for update 2.0

--- build/backEnd.py
+++ build/backEnd.py
@@ -1,19 +1,20 @@
 from build.connection import *
-
+from typing import Dict,List,Optional,Union
 conn = connection()
 col = createDb(conn,'DiscordBot','UserInfo')
+quoteCol = createDb(conn,"DiscordBot","Quotes")
 
 def addVal(guildId,UserId,muteStatus=False,banStatus=False):
     col.insert_one({'guildId':guildId,'UserId':UserId,'muteStatus':muteStatus})
 
 def checkExist(guildId, UserId):
-    count = col.find({"$and":[{'guildId':guildId},{'UserId':UserId}]}).count()
+    count = col.count_documents({"$and":[{'guildId':guildId},{'UserId':UserId}]})
     if count == 0:
         print("Adding someone new!")
         addVal(guildId,UserId)
 
 def checkMuted(guildId, UserId) -> bool:
-    muted = col.find({"$and":[{'guildId':guildId},{'UserId':UserId},{'muteStatus':True}]}).count()
+    muted = col.count_documents({"$and":[{'guildId':guildId},{'UserId':UserId},{'muteStatus':True}]})
     if muted >=1:
         return True
     return False
@@ -30,3 +31,55 @@ def messageIncrement(guildId, UserId):
 
 def messageDecrement(guildId, UserId):
     col.update_one({"$and":[{'guildId':guildId},{'UserId':UserId}]},{'$inc':{'messageCount':-1}})
+
+"""All of the Connection Code for Quotes are below this line"""
+
+def updateQuoteImage(movie:str, imageUrl:str):
+    quoteCol.update_one({
+        "movie":movie
+    },
+        {
+            "$set": {"imageUrl":imageUrl}
+        })
+
+# flags: = None
+def addQuote(movie:str,character:str,quote,type:str,imageUrl:str=None):
+        checkExisting = quoteCol.count_documents({
+            "$and": [
+                {'movie': movie},
+                {'character': character},
+
+            ]
+        }
+        )
+
+        if checkExisting == 0:
+            quoteCol.insert_one({
+                'movie':movie,
+                'character':character,
+                'quote':quote,
+                'imageUrl':imageUrl,
+                'type':type
+
+            })
+        else:
+            quoteCol.update_one(
+                {"$and":[
+                    {
+                        'movie': movie
+                    }, {
+                        'character': character
+                    }
+                ]
+                },
+                {
+                    "$addToSet": {
+                        "quote": {
+                            "$each": quote
+                        }
+                    }
+                }
+            )
+
+def getRandomItem(noOfDocuments:int=1):
+    return [i for i in quoteCol.aggregate([{'$sample':{'size':noOfDocuments}}])][0]
