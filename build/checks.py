1450294fc13f30fc92c39b249f65d204afaba615
Rodrous <rodrous114@gmail.com>
2022-01-15 18:08:16 +0530

    Refactored Checks File

--- build/checks.py
+++ build/checks.py
@@ -1,27 +1,32 @@
+from typing import Dict, Set
+
 from discord.ext import commands
-import discord
-"""    Personal    """
-def devlist():
+
+
+def devlist() -> Dict[str, int]:
     return {
-        #Devs
+        # Devs
         "Giraffe": 323457305855262721,
         "Ppt": 311919809895858177,
         "Finix": 239762173440557057,
-        #Alts
+        # Alts
         "Giraffe | Gif": 701049734382485615,
     }
 
+
 def devsOnly(*args):
-    def predicate(ctx):
-        devs = devlist()
-        devs = set(devs.values())
+    def predicate(ctx) -> bool:
+        devs: Dict[str, int] = devlist()
+        devs: Set = set(devs.values())
         return devs.__contains__(ctx.author.id)
+    
     return commands.check(predicate)
 
+
 def private(*args):
-    def predicate(ctx):
-        devs = devlist()
-        whitelist = {
+    def predicate(ctx) -> bool:
+        devs: Dict[str, int] = devlist()
+        whitelist: Dict[str, int] = {
             # Friends
             "Karma": 564921559836393481,
             "Artsy": 793706755950903306,
@@ -33,14 +38,16 @@ def private(*args):
             "Karma | EllaJD": 845876972222808065
         }
         whitelist.update(devs)
-        whitelist = set(whitelist.values())
+        whitelist: Set = set(whitelist.values())
         return whitelist.__contains__(ctx.author.id)
+    
     return commands.check(predicate)
 
-"""      Specific     """
-def Feedback_n_bug_blacklist(*args):
+
+def feedback_n_bug_blacklist(*args):
     def predicate(ctx):
-        blacklist = {}
-        blacklist = set(blacklist.values())
+        blacklist: Dict = {}
+        blacklist: Set = set(blacklist.values())
         return ctx.author.id not in blacklist
+    
     return commands.check(predicate)
