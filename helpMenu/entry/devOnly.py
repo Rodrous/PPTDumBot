0a3cc34e1bd8b2473627eec0e1658abeb5f674d7
EvilGiraffes <85831955+EvilGiraffes@users.noreply.github.com>
2022-01-16 17:32:11 +0100

    Forgot to add the entries

---
+++ helpMenu/entry/devOnly.py
@@ -0,0 +1,17 @@
+from helpMenu.helpMenuEntry import EntryFactory
+from helpMenu.commands import RestrictedCategory
+
+
+async def Init():
+    """
+    Entry For Commands
+    HelpMenuEntry:
+        :Param Category: Category of type CommandCategory
+        :Param Name: Name of the command
+        :Param Brief: A brief desc about the command
+        :Param Desc: A detailed description of the command
+        :Param Aliases: Optional list of aliases
+        :Param Syntax: Optional Syntax details, will only need the flags at the end example [User: Id, Name] [Number] [Message -> Optional]
+    """
+    entry = EntryFactory(RestrictedCategory.DevOnly)
+    """  NOT INITIATED YET!!  """
